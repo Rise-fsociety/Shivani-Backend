89ced4db7522892e34045872c5f80f3ed41c86de
Rise-fsociety <yhlasashyrov21@gmail.com>
2026-06-26 19:56:30 +0500

    cotroller

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -19,5 +19,11 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique:true,
+    lowercase: true,
+    trim:true
   },
-});
+}, {timestamps: true}
+);
+
+export const User = mongoose.model("User", userSchema)
